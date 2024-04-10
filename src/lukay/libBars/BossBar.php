51be6b5fa51b24e863256d00dbb41d56a8fbfbcc
Luka <127244631+LUKAY207@users.noreply.github.com>
2024-04-10 09:48:23 +0200

    Initial

--- src/lukay/libBars/BossBar.php
+++ src/lukay/libBars/BossBar.php
@@ -4,6 +4,7 @@ namespace lukay\libBars;
 
 use pocketmine\entity\Entity;
 use pocketmine\network\mcpe\protocol\BossEventPacket;
+use pocketmine\player\Player;
 
 class BossBar {
 
@@ -14,21 +15,26 @@ class BossBar {
      * However, this one should be simpler to use.
      */
 
-    public function __construct(string $title, float $healthPercent, int $color) {
+    public function __construct(string $title, bool $darkenScreen = false, float $healthPercent = 100.0, int $color = 0, int $overlay = 0) {
         $this->bossEventPacket = new BossEventPacket();
         $this->bossEventPacket->bossActorUniqueId = Entity::nextRuntimeId();
         $this->bossEventPacket->title = $title;
         $this->bossEventPacket->healthPercent = $healthPercent;
+        $this->bossEventPacket->darkenScreen = $darkenScreen;
         $this->bossEventPacket->color = $color;
+        $this->bossEventPacket->overlay = $overlay;
     }
 
-    public function show(): void {
+    public function show(Player $player): void {
+        BossEventPacket::registerPlayer($this->bossEventPacket->bossActorUniqueId, $player->getId());
         BossEventPacket::show
         (
-            $this->bossEventPacket->bossActorUniqueId,
+            $player->getId(),
             $this->bossEventPacket->title,
             $this->bossEventPacket->healthPercent,
-            $this->bossEventPacket->color
+            $this->bossEventPacket->darkenScreen,
+            $this->bossEventPacket->color,
+            $this->bossEventPacket->overlay
         );
     }
 }
